2421fd74b8bd558abd221fb26c9a5b3de7d74a33
Lilly-Dev(Retired) <83188516+jossgamerYT156@users.noreply.github.com>
2024-04-19 19:45:39 -0700

    Tuto opening-browsers.jl
    some tutorial about using browser(") function

---
+++ J-Lang/Code_examples/opening-browsers.jl
@@ -0,0 +1,6 @@
+print("this is a function for text)
+print("next ahead is a function for opening a browser)
+browser("https://google.com/)
+msg("as you can see, this says 'browser', which means it will open a web browser!)
+msg("and with that browser...well, you can do many things, opening localhost, a simple website you like or just spamming it to annoy friends)
+msg("the possibilities are endless!)
