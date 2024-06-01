2f38f18db9661dd9b895a5ad01ecaa5a6270b8bc
Lilly-Dev <83188516+jossgamerYT156@users.noreply.github.com>
2024-06-01 14:02:46 -0700

    Create main.cpp

---
+++ C/Qt_Based_IDE/main.cpp
@@ -0,0 +1,26 @@
+// main file that loads the Window and strarts the needed services for J-IDE to work properly...
+// it just calls the needed functions from jide.cpp.
+
+#include "jide.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "J-IDE_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    JIDE w;
+    w.show();
+    return a.exec();
+}
